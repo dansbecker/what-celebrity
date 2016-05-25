67414c148bc9b9a814ffd7a47dc3780392c8e965
Dan Becker <dansbecker@gmail.com>
2016-05-24 20:54:55 -0600

    Use bigger network. Val loss around 3.85, val_acc around 22%

--- model.py
+++ model.py
@@ -2,9 +2,9 @@ import json
 import pandas as pd
 import numpy as np
 from keras.utils import np_utils
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers.convolutional import Convolution1D, Convolution2D, MaxPooling2D, Convolution3D
+from keras.models import Sequential, model_from_json
+from keras.callbacks import EarlyStopping
+from keras.layers import Dense, Dropout, Flatten, Convolution1D, Convolution2D, MaxPooling2D, Convolution3D
 from keras.layers.noise import GaussianNoise
 from keras.objectives import categorical_crossentropy
 from keras.optimizers import Adagrad, Adam, Adamax, RMSprop
@@ -13,35 +13,39 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.utils import shuffle
 from sklearn.cross_validation import train_test_split
 
-
-def model_from_thumbnails(train_x, val_x, train_y, val_y):
-    n_obs, n_rows, n_cols, n_channels = train_x.shape
+def model_from_thumbnails(train_x, train_y, val_x, val_y):
+    n_obs, n_channels, n_rows, n_cols = train_x.shape
     n_classes = y.shape[1]
 
     model = Sequential()
-    model.add(Convolution2D(3, 1, 1, init="glorot_uniform",
-                            activation='relu', border_mode='valid',
-                            dim_ordering='th',
-                            input_shape=(n_rows, n_cols, n_channels)))
-    model.add(Convolution2D(8, 2, 2, border_mode='same', init="glorot_uniform",
-                            dim_ordering='th', activation='relu',
-                            input_shape=(n_rows, n_cols)))
-    model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='th'))
-    model.add(Convolution2D(16, 2, 2, border_mode='same', init="glorot_uniform",
-                            dim_ordering='th', activation='relu'))
+    model.add(Convolution2D(32, 2, 2, border_mode='valid',
+                            activation='relu',
+                            input_shape=(n_channels, n_rows, n_cols)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(64, 2, 2, border_mode='valid',
+                            activation='relu'))
+    model.add(Convolution2D(64, 2, 2, border_mode='valid',
+                            activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(64, 2, 2, border_mode='valid',
+                            activation='relu'))
+
     model.add(Flatten())
+    model.add(Dropout(0.5))
     model.add(Dense(100, activation='relu'))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(n_classes, activation='softmax'))
     optimizer = Adam()
     model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-    model.fit(train_x, train_y, batch_size=32, nb_epoch=15, shuffle=True, validation_data=(val_x, val_y))
-    val_preds = model.predict_proba(val_x)
-    # model.loss returns tensor from the backend
-    val_loss = model.loss(val_y, val_preds).eval().mean()
-    return val_loss
+    stopper = EarlyStopping(monitor='val_loss', patience=15, verbose=0, mode='auto')
 
+    model.fit(train_x, train_y, shuffle=True,
+                        nb_epoch=100, validation_data=(val_x, val_y),
+                        callbacks = [stopper])
+    return model
 
 if __name__ == "__main__":
     loaded_file = np.load("work/thumbnail_px_and_targ.npy.npz")
@@ -50,4 +54,13 @@ if __name__ == "__main__":
     y = LabelBinarizer().fit_transform(loaded_file['y'])
     train_x, val_x, train_y, val_y = train_test_split(x, y,
                                                       test_size=0.10, random_state=80401)
-    model_from_thumbnails(train_x, val_x, train_y, val_y)
+
+    model = model_from_thumbnails(train_x, train_y, val_x, val_y)
+    print('Saving Model...')
+    json_string = model.to_json()
+    open('my_model_architecture.json', 'w').write(json_string)
+    model.save_weights('my_model_weights.h5', overwrite=True)
+
+    print('Reloading Model...')
+    model = model_from_json(open('my_model_architecture.json').read())
+    model.load_weights('my_model_weights.h5')
